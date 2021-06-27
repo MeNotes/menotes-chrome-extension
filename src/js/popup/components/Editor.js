84aaf6cbce15ed117a827861dd3035a8172e6210
Sergey <sperezhogin@griddynamics.com>
2021-06-27 21:42:11 +0200

    feat: editor. autofocus is added, status bar is removed.

--- src/js/popup/components/Editor.js
+++ src/js/popup/components/Editor.js
@@ -1,49 +1,58 @@
-import SimpleMDE from 'simplemde';
-import 'simplemde/dist/simplemde.min.css'
+import SimpleMDE from "simplemde";
+import "simplemde/dist/simplemde.min.css";
 
 export class Editor {
-    constructor(onSave, onChange) {
-        this.setNote = this.setNote.bind(this);
-        this._copyTextToClipboard = this._copyTextToClipboard.bind(this);
-        this._onChangeHandler = this._onChangeHandler.bind(this);
+  constructor(onSave, onChange) {
+    this.setNote = this.setNote.bind(this);
+    this._copyTextToClipboard = this._copyTextToClipboard.bind(this);
+    this._onChangeHandler = this._onChangeHandler.bind(this);
 
-        this.simplemde = new SimpleMDE({ element: document.getElementById('editor') });
-        this.titleInput = document.getElementById('note-title-input');
-        const copyButton = document.getElementById('copy-note');
-        const saveButton = document.getElementById('save-note');
+    this.simplemde = new SimpleMDE({
+      element: document.getElementById("editor"),
+      autofocus: true,
+      showIcons: ["code"],
+      status: false,
+    });
 
-        copyButton.addEventListener('click', () => {
-            this._copyTextToClipboard(this.getEditorValue());
-        })
+    this.titleInput = document.getElementById("note-title-input");
+    const copyButton = document.getElementById("copy-note");
+    const saveButton = document.getElementById("save-note");
 
-        saveButton.addEventListener('click', () => {
-            onSave({ title: this.titleInput.value, value: this.simplemde.value() });
-        })
+    copyButton.addEventListener("click", () => {
+      this._copyTextToClipboard(this.getEditorValue());
+    });
 
-        this.titleInput.addEventListener('change', this._onChangeHandler(onChange));
-        this.simplemde.codemirror.on("change", this._onChangeHandler(onChange));
+    saveButton.addEventListener("click", () => {
+      onSave({ title: this.titleInput.value, value: this.simplemde.value() });
+    });
+
+    this.titleInput.addEventListener("change", this._onChangeHandler(onChange));
+    this.simplemde.codemirror.on("change", this._onChangeHandler(onChange));
+  }
+
+  setNote({ title, value }) {
+    this.titleInput.value = title;
+    this.simplemde.value(value);
+  }
+
+  _copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+      console.error("Clipborad is not available", err);
+      return;
     }
+    navigator.clipboard.writeText(text).then(
+      function () {
+        console.log("Async: Copying to clipboard was successful!");
+      },
+      function (err) {
+        console.error("Async: Could not copy text: ", err);
+      }
+    );
+  }
 
-    setNote({ title, value }) {
-        this.titleInput.value = title
-        this.simplemde.value(value);
-    }
-
-    _copyTextToClipboard(text) {
-        if (!navigator.clipboard) {
-            console.error('Clipborad is not available', err);
-            return;
-        }
-        navigator.clipboard.writeText(text).then(function () {
-            console.log('Async: Copying to clipboard was successful!');
-        }, function (err) {
-            console.error('Async: Could not copy text: ', err);
-        });
-    }
-
-    _onChangeHandler(callback) {
-        return () => {
-            callback({ title: this.titleInput.value, value: this.simplemde.value() });
-        }
-    }
+  _onChangeHandler(callback) {
+    return () => {
+      callback({ title: this.titleInput.value, value: this.simplemde.value() });
+    };
+  }
 }
