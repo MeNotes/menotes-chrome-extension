ab5721dfd6293bbff1d2d3d6efa1ce8cb894aebc
Sergey <sperezhogin@griddynamics.com>
2021-07-09 11:22:20 +0200

    fix scroll issues

--- src/js/popup/containers/NotesContainer.js
+++ src/js/popup/containers/NotesContainer.js
@@ -66,7 +66,7 @@ export class NotesContainer {
         const title = this._getNoteTitle(value.trim());
         this.notesContainer.innerHTML += `
           <div class="note-item" data-id="${id}" data-action="${OPEN_ACTION}">
-              <div class="note-item__title" data-id="${id}" data-action="${OPEN_ACTION}">${title}</div>
+              <div class="note-item__title" title="${title}" data-id="${id}" data-action="${OPEN_ACTION}">${title}</div>
               <div class="note-item__actions">
               <button class="button button--icon" data-id="${id}" data-action="${REMOVE_ACTION}">
                   <i class="fa fa-trash"></i>
