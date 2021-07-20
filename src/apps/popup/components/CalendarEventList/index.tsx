665e5543fddf436d5e17ac43449a8b40fff1695c
Tkach7 <tkach.spam@yandex.ru>
2021-07-20 23:13:20 +0500

    added  calednar events

--- src/apps/popup/components/CalendarEventList/index.tsx
+++ src/apps/popup/components/CalendarEventList/index.tsx
@@ -1,20 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  GoogleCalendarService,
+  StorageService,
+} from "../../../../shared/services";
 import styles from "./styles.module.css";
 
+function useCalendarEvents() {
+  const [loading, setLoading] = useState(false);
+  const [calendarEvents, setCalendarEvents] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const storage = new StorageService();
+    const calendarService = new GoogleCalendarService(storage);
+
+    calendarService
+      .getCalendarEvents()
+      .then((events: any) => {
+        setCalendarEvents(events);
+      })
+      .catch(setError)
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return { loading, data: calendarEvents, error };
+}
+
 export const CalendarEventList = () => {
+  const { data: events, loading, error } = useCalendarEvents();
+
   return (
     <section className={styles.container}>
-      <div id="calendar-event-loader" className="calendar-event-list">
-        Loading...
+      {loading && (
+        <div id="calendar-event-loader" className={styles.loader}>
+          Loading...
+        </div>
+      )}
+      {error && (
+        <div id="calendar-event-error">
+          <i className="fa fa-exclamation-triangle"></i>
+          Events are not available
+        </div>
+      )}
+      <div id="calendar-event-list" className={styles.calendar_event_list}>
+        {events && events.length
+          ? events.map((event: any) => (
+              <button className={styles.list_item} data-event-id={event.id}>
+                ${event.summary}
+              </button>
+            ))
+          : null}
       </div>
-      <div id="calendar-event-error" className="calendar-event-list hidden">
-        <i className="fa fa-exclamation-triangle"></i>
-        Events are not available
-      </div>
-      <div
-        id="calendar-event-list"
-        className="calendar-event-list hidden"
-      ></div>
     </section>
   );
 };
