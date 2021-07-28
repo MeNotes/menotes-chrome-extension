0c5d47898995d1904e34148fc7bb56d6b17707f0
Sergey <sperezhogin@griddynamics.com>
2021-07-29 00:02:45 +0200

    feature: calendar events create new note

--- src/apps/popup/components/CalendarEventList/index.tsx
+++ src/apps/popup/components/CalendarEventList/index.tsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { useStoreon } from "storeon/react";
+import { Note } from "../../../../shared/models";
 import {
-  CalendarEventsEvents,
-  CalendarEventsState,
+  useCalendarEventsQuery,
+  useNotesMutation,
 } from "../../../../store/modules";
 import styles from "./styles.module.css";
 
 export const CalendarEventList = () => {
-  const { events, error, loading } = useStoreon<
-    CalendarEventsState,
-    CalendarEventsEvents
-  >("events");
+  const { data, loading, error } = useCalendarEventsQuery();
+  const { upsertActiveNote } = useNotesMutation();
+
+  const onEventClick = (event: any) => {
+    upsertActiveNote(new Note({ id: null, value: event.summary }));
+  };
+
   return (
     <section className={styles.container}>
       {loading && (
@@ -18,20 +21,26 @@ export const CalendarEventList = () => {
           Loading...
         </div>
       )}
+
       {error && (
         <div id="calendar-event-error">
           <i className="fa fa-exclamation-triangle"></i>
           Events are not available
         </div>
       )}
+
       <div id="calendar-event-list" className={styles.calendar_event_list}>
-        {events && events.length
-          ? events.map((event: any) => (
-              <button className={styles.list_item} key={event.id}>
-                ${event.summary}
-              </button>
-            ))
-          : null}
+        {data &&
+          data.length &&
+          data.map((event: any) => (
+            <button
+              className={styles.list_item}
+              key={event.id}
+              onClick={() => onEventClick(event)}
+            >
+              ${event.summary}
+            </button>
+          ))}
       </div>
     </section>
   );
